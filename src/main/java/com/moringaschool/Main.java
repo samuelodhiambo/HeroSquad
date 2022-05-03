ff4907662157cd6d3deed1329d10bf4f9be2fae3
Samuel Ian <samian@localhost.localdomain>
2022-05-03 13:09:34 +0300

    patch: It will set static file location

--- src/main/java/com/moringaschool/Main.java
+++ src/main/java/com/moringaschool/Main.java
@@ -1,5 +1,22 @@
-package com.moringaschool;public class Main {
+package com.moringaschool;
+import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+            return new ModelAndView(new HashMap(), "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "addHero.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
