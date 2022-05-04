083754f57ecc692c48edaf7b5575f36720f9a661
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-04 07:23:08 +0300

    test: It will add test for Hero class

---
+++ src/test/java/com/moringaschool/HeroTest.java
@@ -0,0 +1,30 @@
+package com.moringaschool;
+
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroTest {
+
+    @After
+    public void tearDown() {
+//        Hero.clearAllPosts(); //clear out all the posts before each test.
+    }
+
+    @Test
+    public void AllHeroesAreCorrectlyReturned_true() {
+        Hero hero = new Hero("John", 22, "Very Strong", "cant run");
+        Hero otherHero = new Hero("Kevin", 30, "Very Intelligent", "cries alot");
+        assertEquals(2, Hero.getAll().size());
+    }
+
+    @Test
+    public void AllHeroesContainsAllHeroes_true() {
+        Hero hero = new Hero("John", 22, "Very Strong", "cant run");
+        Hero otherHero = new Hero("Kevin", 30, "Very Intelligent", "cries alot");
+        assertTrue(Hero.getAll().contains(hero));
+        assertTrue(Hero.getAll().contains(otherHero));
+    }
+
+}
