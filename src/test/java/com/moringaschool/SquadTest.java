57ea4ffdff16ccba47a42e40f57528510f126f96
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-04 09:42:39 +0300

    patch: It will create test methods for SquadTest

--- src/test/java/com/moringaschool/SquadTest.java
+++ src/test/java/com/moringaschool/SquadTest.java
@@ -7,15 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class SquadTest {
     @Test
     public void AllSquadsAreCorrectlyReturned_true() {
-        Squad squad = new Squad("John", 22, "Very Strong", "cant run");
-        Squad otherSquad = new Squad("Kevin", 30, "Very Intelligent", "cries alot");
+        Squad squad = new Squad(1, "Marvel", 1);
+        Squad otherSquad = new Squad(1, "Marvel", 2);
         assertEquals(2, Squad.getAll().size());
     }
 
     @Test
     public void AllHSquadsContainsAllSquads_true() {
-        Squad squad = new Squad("John", 22, "Very Strong", "cant run");
-        Squad otherSquad = new Squad("Kevin", 30, "Very Intelligent", "cries alot");
+        Squad squad = new Squad(1, "Marvel", 1);
+        Squad otherSquad = new Squad(1, "Marvel", 2);
         assertTrue(Squad.getAll().contains(squad));
         assertTrue(Squad.getAll().contains(otherSquad));
     }
