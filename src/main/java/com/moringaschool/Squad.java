dbd43aad4c0364207d34c2d94bb27c1ddff30994
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-04 09:43:16 +0300

    patch: It will create Squad class members and methods

--- src/main/java/com/moringaschool/Squad.java
+++ src/main/java/com/moringaschool/Squad.java
@@ -1,5 +1,23 @@
 package com.moringaschool;
 
-public class Squad {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class Squad {
+    private int maxSize;
+    private String name;
+    private String[] course = {"gender equity", "Evil", "Bullying"};
+    private String fightCourse;
+    public static ArrayList<Squad> instances = new ArrayList<>();
+
+    public Squad(int maxSize, String name, int course){
+        this.maxSize = maxSize;
+        this.name = name;
+        this.fightCourse = this.course[course];
+        instances.add(this);
+    }
+
+    public static ArrayList<Squad> getAll() {
+        return instances;
+    }
 }
