b19661027405f94b49678732e855216a49fd693b
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-04 08:35:28 +0300

    patch: It will add post method for saving new heroes

--- src/main/java/com/moringaschool/Main.java
+++ src/main/java/com/moringaschool/Main.java
@@ -3,6 +3,7 @@ import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,11 +15,14 @@ public class Main {
             Hero hero1 = new Hero("Superman", 99, "Flying", "Not invincible");
             Hero hero2 = new Hero("Batman", 80, "Can't see", "Allergic to light");
 
-            Map<String, Object> model = new HashMap<>();
+            Map<String, Object> model = new HashMap<String, Object>();
 
-            request.session().attribute("heroes", Hero.getAll());
+            ArrayList<Hero> heroes = Hero.getAll();
 
-            model.put("heroes", request.session().attribute("heroes"));
+            request.session().attribute("heros", heroes);
+            System.out.println(heroes);
+
+            model.put("heroes", request.session().attribute("hero"));
 
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
@@ -26,6 +30,18 @@ public class Main {
         get("/form", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "addHero.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/add", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            int age = Integer.parseInt(request.queryParams("age"));
+            String power = request.queryParams("power");
+            String weakness = request.queryParams("weakness");
+            Hero newHero = new Hero(name, age, power, weakness);
+            request.session().attribute("hero", newHero);
+            model.put("hero", newHero);
+            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
